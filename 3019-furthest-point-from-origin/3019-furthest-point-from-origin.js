60d7fe9d9331a565738a39cf0e23279c4def4c24
Papuna Fshaveli <papunafshaveli@gmail.com>
2023-09-30 20:54:45 +0400

    Time: 69 ms (36.41%), Space: 44.9 MB (7.82%) - LeetHub

--- 3019-furthest-point-from-origin/3019-furthest-point-from-origin.js
+++ 3019-furthest-point-from-origin/3019-furthest-point-from-origin.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 var furthestDistanceFromOrigin = function(moves) {
-    left = 0;
-    right = 0;
+    x = 0;
+    y = 0;
     for (let i = 0; i < moves.length; i++){
         if (moves[i] === "L"){
-            left --;
-            right --;
+            x ++;
+            y ++;
         } else if (moves[i] === "R" ){
-            left ++;
-            right ++;
+            x --;
+            y --;
         } else{
-            left --;
-            right ++;
+            x ++;
+            y --;
         }
     }
-    return Math.max(Math.abs(left), Math.abs(right))
+    return Math.max(Math.abs(x), Math.abs(y))
 };
