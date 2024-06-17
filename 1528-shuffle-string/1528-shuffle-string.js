aeb9826095aa31401c27a5c09b175fd5b9e45ca1
Papuna Fshaveli <papunafshaveli@gmail.com>
2024-06-17 10:14:02 +0400

    Time: 45 ms (97.44%), Space: 50.8 MB (28.82%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+   let arr = new Array(s.length);
+    
+    for (let i = 0; i < s.length; i++){
+        arr[indices[i]] = s[i];
+    }
+    return arr.join("");
+};
